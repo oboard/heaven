1d77ab90e41d65af183b6410db7b207bb773828d
oboard <oboard@outlook.com>
2024-07-31 15:20:03 +0800

    fix: script

--- examples/http-server/start.sh
+++ examples/http-server/start.sh
@@ -1,7 +1,7 @@
 moon build --target=wasm
 mkdir -p ./target/app/node
 cp -rf ./node/ ./target/app/node/
-cp ./target/wasm/release/build/main/main.wasm ./target/app/node/main.wasm
+cp ./target/wasm/release/build/main/main.wasm ./target/app/node/assets/app.wasm
 cd ./target/app/node/
 npm install
 npm start
